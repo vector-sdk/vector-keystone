af6df2eab046fac5cc257e40aa1899f7eb6bc5d5
Markku Kylänpää <markku.kylanpaa@vtt.fi>
2025-08-19 10:57:33 +0300

    qemu: Add new 'debug' and 'ddd-connect' targets to run.mk
    - Introduced a 'debug' target that launches QEMU and halts
      immediately, waiting for a debugger connection.
    - Added a 'ddd-connect' target to facilitate connecting to QEMU using
      the DDD debugger.
    - Updated QEMU flags to allocate 4GB of memory.
    - Adjusted root filesystem device interface to align with QEMU 10
      changes.
    
    Signed-off-by: Markku Kylänpää <markku.kylanpaa@vtt.fi>

--- mkutils/plat/generic/run.mk
+++ mkutils/plat/generic/run.mk
@@ -6,17 +6,16 @@ KEYSTONE_PORT   ?= 9821
 QEMU_DBG_PORT   ?= $(shell echo $$(( $(KEYSTONE_PORT) + 1)) )
 QEMU_DEBUG      := -gdb tcp::$(QEMU_DBG_PORT) -S
 
-QEMU_MEM        ?= 2G
+QEMU_MEM        ?= 4G
 QEMU_SMP        ?= 4
-
 
 QEMU_FLAGS := -m $(QEMU_MEM) -smp $(QEMU_SMP) -nographic \
                 -machine virt,rom=$(BUILDROOT_BUILDDIR)/images/bootrom.bin \
                 -bios $(BUILDROOT_BUILDDIR)/images/fw_jump.elf \
                 -kernel $(BUILDROOT_BUILDDIR)/images/Image \
-                -drive file=$(BUILDROOT_BUILDDIR)/images/rootfs.ext2,format=raw,id=hd0 \
+                -drive file=$(BUILDROOT_BUILDDIR)/images/rootfs.ext2,format=raw,id=hd0,if=none \
                 -device virtio-blk-device,drive=hd0 \
-                -append "console=ttyS0 ro root=/dev/vda" \
+                -append "console=ttyS0 ro rootwait root=/dev/vda" \
                 -netdev user,id=net0,net=192.168.100.1/24,dhcpstart=192.168.100.128,hostfwd=tcp::$(KEYSTONE_PORT)-:22 \
                 -device virtio-net-device,netdev=net0 \
                 -device virtio-rng-pci \
@@ -29,8 +28,18 @@ run:
 	$(call log,info,Starting QEMU)
 	$(BUILDROOT_BUILDDIR)/host/bin/qemu-system-riscv$(KEYSTONE_BITS) $(QEMU_FLAGS)
 
+debug:
+	$(call log,info,Debugging QEMU)
+	$(BUILDROOT_BUILDDIR)/host/bin/qemu-system-riscv$(KEYSTONE_BITS) $(QEMU_FLAGS) $(QEMU_DEBUG)
+
 debug-connect:
 	$(call log,info,Connecting to QEMU)
 	$(BUILDROOT_BUILDDIR)/host/bin/riscv$(KEYSTONE_BITS)-buildroot-linux-gnu-gdb \
                 -iex "set KEYSTONE=$(KEYSTONE)" \
                 -x $(KEYSTONE)/scripts/gdb/generic.cfg
+
+ddd-connect:
+	$(call log,info,Connecting ddd to QEMU)
+	ddd --debugger '$(BUILDROOT_BUILDDIR)/host/bin/riscv$(KEYSTONE_BITS)-buildroot-linux-gnu-gdb \
+                -iex "set KEYSTONE=$(KEYSTONE)" \
+                -x $(KEYSTONE)/scripts/gdb/generic.cfg'
